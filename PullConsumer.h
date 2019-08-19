8afd85fdbbbde61d173a6adae46171e05e8ebffe
Li Zhanhui <shutian.lzh@alibaba-inc.com>
2019-08-19 16:04:25 +0800

    Fix incorrect use of const specifier

--- PullConsumer.h
+++ PullConsumer.h
@@ -1,6 +1,7 @@
 #ifndef __PULLCONSUMER_H__
 #define __PULLCONSUMER_H__
 
+#include <stdexcept>
 #include <string>
 #include <vector>
 #include "MessageQueueONS.h"
@@ -15,24 +16,19 @@ class ONSCLIENT_API PullConsumer {
   PullConsumer() {}
   virtual ~PullConsumer() {}
 
-  virtual void start() = 0;
-  virtual void shutdown() = 0;
-  virtual void fetchSubscribeMessageQueues(
-      const std::string& topic, std::vector<MessageQueueONS>& mqs) = 0;
-  virtual PullResultONS pull(const MessageQueueONS& mq,
-                             const std::string& subExpression, long long offset,
-                             int maxNums) = 0;
-  virtual long long searchOffset(const MessageQueueONS& mq, long long timestamp) = 0;
-  virtual long long maxOffset(const MessageQueueONS& mq) = 0;
-  virtual long long minOffset(const MessageQueueONS& mq) = 0;
-  virtual void updateConsumeOffset(const MessageQueueONS& mq,
-                                   long long offset) = 0;
-  virtual void removeConsumeOffset(const MessageQueueONS& mq) = 0;
-  virtual long long fetchConsumeOffset(const MessageQueueONS& mq,
-                                       bool fromStore) = 0;
-  virtual void persistConsumerOffset4PullConsumer(const MessageQueueONS& mq)
-      throw(ons::ONSClientException) = 0;
+  virtual void start()                                                                                  = 0;
+  virtual void shutdown()                                                                               = 0;
+  virtual void fetchSubscribeMessageQueues(const std::string& topic, std::vector<MessageQueueONS>& mqs) = 0;
+  virtual PullResultONS pull(const MessageQueueONS& mq, const std::string& subExpression, long long offset,
+                             int maxNums)                                                               = 0;
+  virtual long long searchOffset(const MessageQueueONS& mq, long long timestamp)                        = 0;
+  virtual long long maxOffset(const MessageQueueONS& mq)                                                = 0;
+  virtual long long minOffset(const MessageQueueONS& mq)                                                = 0;
+  virtual void updateConsumeOffset(const MessageQueueONS& mq, long long offset)                         = 0;
+  virtual void removeConsumeOffset(const MessageQueueONS& mq)                                           = 0;
+  virtual long long fetchConsumeOffset(const MessageQueueONS& mq, bool fromStore)                       = 0;
+  virtual void persistConsumerOffset4PullConsumer(const MessageQueueONS& mq) noexcept(false)            = 0;
 };
 
-}
+}  // namespace ons
 #endif
